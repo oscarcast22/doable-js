d359d7d6af8e5dee727dcced5378f390e4964059
Oscar Castillo <oscar_cm@outlook.com>
2023-02-23 21:24:23 -0600

    Create tasks services

---
+++ scripts/services/tasks-services.js
@@ -0,0 +1,13 @@
+import apiFetch from "./api-fetch,js";
+
+export function getTasks() {
+    return apiFetch("tasks")
+}
+
+export function createTask(newTask = {title, due_date}) {
+    return apiFetch("tasks", {body: newTask})
+}
+
+export function deleteTask(id) {
+    return apiFetch(`tasks/${id}`)
+}
